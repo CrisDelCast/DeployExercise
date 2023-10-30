1a2260608d00574db31f7e64b803ae33d4d2fd72
Cristian David Del Castillo Garcia <1002967791@u.icesi.edu.co>
2023-10-30 16:07:49 -0500

    feat-taller: se añadio todo lo que se dice en el taller

--- p1sisworkshop/settings.py
+++ p1sisworkshop/settings.py
@@ -23,10 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-w3($fz7!(kr23d_a7lqm8^_tq@xf_so#z5lb&eyt2s$%8ea1$('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
+DEBUG = False
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -75,11 +73,17 @@ WSGI_APPLICATION = 'p1sisworkshop.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+  'default': {
+    'ENGINE': 'django.db.backends.postgresql',
+    'NAME': 'domi',
+    'USER': 'domiciano.rincon',
+    'PASSWORD': 'GT1DF9lbiEXm',
+    'HOST': 'ep-holy-mud-52208127.us-east-2.aws.neon.tech',
+    'PORT': '5432',
+    'OPTIONS': {'sslmode': 'require'},
+  }
 }
+
 
 
 # Password validation
@@ -115,7 +119,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-
+import os
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 STATIC_URL = 'static/'
 LOGIN_URL = '/signin'
 
